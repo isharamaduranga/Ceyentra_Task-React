fc3bc3fc5c853fcaa6e960ad202637c57164de08
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-07-14 01:46:58 +0530

    Created SignUp component in Pages Folder ....

---
+++ src/pages/Signup/Signup.js
@@ -0,0 +1,150 @@
+import React, {useState} from 'react';
+import './Signup.css'
+import {useFormik} from "formik";
+import {Box} from "@mui/material";
+import {BiUserPin} from "react-icons/bi";
+import Input from "../../components/UI/Input/input";
+import {Link, useNavigate} from "react-router-dom";
+import {createUserWithEmailAndPassword} from 'firebase/auth';
+import {auth} from "../../config/firebase";
+import {toast} from 'react-toastify';
+
+
+function Signup(props) {
+    const navigate = useNavigate();
+    const [submitButtonDisabled, setSubmitButtonDisabled] = useState(false);
+
+    const initialValues = {
+        name: "", email: "", password: ""
+    }
+    const validate = (values) => {
+        let errors = {}
+
+        if (!values.name) {
+            errors.name = "Name Can Not be Blank";
+        }
+        if (!values.email) {
+            errors.email = "E-Mail Can Not be Blank";
+        }
+        if (!values.password) {
+            errors.password = "Password is empty , Check Again!!"
+        }
+        return errors;
+    }
+
+    const onSubmit = (values, {resetForm}) => {
+
+        console.log(values);
+
+        setSubmitButtonDisabled(true);
+        createUserWithEmailAndPassword(auth, values.email, values.password).then((res) => {
+            console.log(res);
+            toast.success(`${values.name} Sign-Up Successfully ...`)
+            setSubmitButtonDisabled(false);
+            resetForm({values:''})
+            navigate("/login");
+
+        }).catch((err) => {
+            toast.error(err.message)
+            setSubmitButtonDisabled(false);
+            console.log(err);
+        })
+
+        /*
+
+              const apiURL = `https://task-list-5a410-default-rtdb.firebaseio.com/tasks/${taskId}.json`;
+
+              const task = {
+                  ...values,
+                  status: 'New',
+                  id: taskId,
+              }
+              axios.put(apiURL, task).then((res) => {
+                  if (res.status === 200) {
+                      setMsg('Task Saved Successfully ...');
+                      resetForm({values:''})
+                  }
+              }).catch((err) => {
+                  setMsg('Something Went wrong Not Saved !!!')
+              })*/
+    }
+
+
+    const formik = useFormik({
+        initialValues, onSubmit, validate,
+    });
+
+
+    return (<div className="d-flex  align-items-center justify-content-center  " style={{height: '84vh'}}>
+
+        <Box
+            className="shadow-lg rounded-4 p-4"
+            component="form"
+            sx={{
+                '& .MuiTextField-root': {m: 1, width: '20vw', height: '9vh'},
+            }}
+            noValidate
+            autoComplete="off"
+            onSubmit={formik.handleSubmit}
+        >
+
+            <div className='text-center'>
+                <BiUserPin className="pb-1 text-primary text-center" size="55px"/>
+            </div>
+            <h3 className="text-center">SignUp Form</h3>
+            <br/>
+
+            <Input
+                label="Name"
+                id="outlined-size-small"
+                name="name"
+                value={formik.values.name}
+                size="small"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                helperText={formik.errors.name && formik.touched.name ?
+                    <span className="small text-danger">{formik.errors.name}</span> : null}
+            />
+
+            <Input
+                label="E-Mail"
+                id="outlined-size-small"
+                name="email"
+                value={formik.values.email}
+                size="small"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                helperText={formik.errors.email && formik.touched.email ?
+                    <span className="small text-danger">{formik.errors.email}</span> : null}
+            />
+
+            <Input
+                label="Password"
+                id="outlined-password-input"
+                name="password"
+                value={formik.values.password}
+                size="small"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                helperText={formik.errors.password && formik.touched.password ?
+                    <span className="small text-danger">{formik.errors.password}</span> : null}
+                type='password'
+                autoComplete="current-password"
+            />
+
+            <div className="d-flex flex-wrap align-items-center justify-content-center">
+                <button
+                    type="submit"
+                    className="btn mt-1 btn-success align-items-center"
+                    disabled={submitButtonDisabled}
+                >
+                    Submit
+                </button>
+            </div>
+            <p className='pt-3 m-0 text-center'>Already Have an Account ?</p>
+            <div className='text-center'><Link to='/login' className='badge bg-info'>Login</Link></div>
+        </Box>
+    </div>);
+}
+
+export default Signup;
