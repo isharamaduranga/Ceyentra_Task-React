3e0a32c3cb12535312a6155fe34afca9ddb6cc11
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-07-14 01:46:23 +0530

    Created PageNotFound component in Pages Folder ....

---
+++ src/pages/PageNotFound/PageNotFound.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import notFoundImg from "../../assets/errorpage.webp";
+import {Link} from 'react-router-dom'
+const PageNotFound = () => {
+    return (
+        <div className="text-center mt-5 h-100">
+            <img className="not_found_image pt-5 w-25 " src={notFoundImg} alt='not_found_image' />
+            <h3 className="pb-5">Oops ! Page Not Found !!!</h3>
+
+            <Link to="/login" className="pnf-btn">Go Back Home</Link>
+        </div>
+    );
+};
+
+export default PageNotFound;
