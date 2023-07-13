e473ebf1204ae8305e6b8fba73247a815bbb2896
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-07-14 01:49:13 +0530

    Created Unique Input Component and Apply React Router Dom & create routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
+
 import 'react-toastify/dist/ReactToastify.css';
 import {ToastContainer} from "react-toastify";
 import {Route,Routes} from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Signup from "./pages/Signup/Signup";
+import PageNotFound from "./pages/PageNotFound/PageNotFound";
+import Header from "./Layout/header/Header";
 
 function App() {
   return (
     <>
         <ToastContainer/>
-
-
-            <h1>Hello React World</h1>
-
+        <Header/>
+        <Routes>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/signup" element={<Signup/>}/>
+            <Route path="/" element={<Home/>}/>
+            <Route path="*" element={<PageNotFound/>}/>
+        </Routes>
 
     </>
   );
---
+++ src/components/UI/Input/input.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import TextField from "@mui/material/TextField";
+
+const Input = (props) => {
+    return (
+        <div>
+            <TextField
+                label={props.label}
+                id={props.id}
+                name={props.name}
+                value={props.value}
+                size={props.size}
+                onChange={props.onChange}
+                onBlur={props.onBlur}
+                helperText={props.helperText}
+                type={props.type}
+                autoComplete={props.autoComplete}
+            />
+        </div>
+    );
+};
+
+export default Input;
